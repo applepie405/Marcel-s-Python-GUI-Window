6d2e5e54809f992d3d839d48b9d71db66764fe78
applepie405 <65572060+applepie405@users.noreply.github.com>
2021-02-12 10:59:31 +1300

    Add files via upload

---
+++ hello-worldj80.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+# Create a window
+root = Tk()
+root.title("My GUI App")
+
+# Create a label and add it to the window using pack()
+labell = Label(root, text="lable2 with longer sentence!")
+label2.pack()
+
+# Run the main window loop
+root.mainloop()
