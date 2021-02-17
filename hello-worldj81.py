ff8cf813d94e199a5e729de61361504b7e207086
applepie405 <65572060+applepie405@users.noreply.github.com>
2021-02-18 09:23:28 +1300

    Add files via upload

---
+++ hello-worldj81.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+# Create a window
+root = Tk()
+root.title("My GUI App")
+
+
+# Run the main window loop
+root.mainloop ()
+
+from tkinter import *
+from tkinter import ttk
+from tkinter import scrolledtext
+
+my_button = ttk.Button(root, text="hello")
