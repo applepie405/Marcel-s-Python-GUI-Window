1b1f1b2481410068663d23a0936ca298800ac21e
applepie405 <65572060+applepie405@users.noreply.github.com>
2021-02-19 10:59:03 +1300

    Add files via upload

---
+++ hello-worldj91.py
@@ -0,0 +1,45 @@
+from tkinter import *
+
+root = Tk()
+root.title("Goal Tracker")
+
+# Create and set the message text variable
+message_text = StringVar()
+message_text.set("Welcome! You can deposit or withdraw money and see your progress towards your goals.")
+
+# Create and pack the message label
+message_label = Label(root, textvariable=message_text, wraplength=250)
+message_label.pack()
+
+# Create the PhotoImage and label to hold it
+neutral_image = PhotoImage(file="/images/python/neutral.png")
+image_label = Label(root, image=neutral_image)
+image_label.pack()
+
+# Create and set the account details variable
+account_details = StringVar()
+account_details.set("Savings: $0 \nTotal balance: $0")
+
+# Create the details label and pack it into the GUI
+details_label = Label(root, textvariable=account_details)
+details_label.pack()
+
+# Create a label for the amount field and pack it into the GUI
+amount_label = Label(root, text="Amount:")
+amount_label.pack()
+
+# Create a variable to store the amount
+amount = DoubleVar()
+amount.set("") 
+
+# Create an entry to type in amount
+amount_entry = Entry(root, textvariable=amount)
+amount_entry.pack()
+
+# Create a submit button
+
+submit_button = Button(root, text="Submit")
+submit_button.pack()
+# Run the mainloop
+root.mainloop()
+
